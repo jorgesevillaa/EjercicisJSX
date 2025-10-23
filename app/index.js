0bf026f55f668d5a8440ac4f23751831dd446f5c
jorgesevillaa <jsevillagenoves@gmail.com>
2025-10-23 15:55:19 +0200

    acabat

--- app/index.js
+++ app/index.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
-import { Button, Icon, Provider as PaperProvider, TextInput, useState } from "react-native-paper";
+import { Button, Icon, Provider as PaperProvider, TextInput} from "react-native-paper";
 
 const estil = "florida";
 const isAdmin = true;
 
 const mostrarIcona = () => {
-  return(
-    <Button mode="contained" icon = "format-list-bulleted">
+  return (
+    <Button mode="contained" icon="format-list-bulleted">
       INFORMES
     </Button>
   )
 }
+const moduls2Dam = [
+  { nom: 'DIN', professor: 'Manel', hores: 120, style: { backgroundColor: "#F48FB1" } },
+  { nom: 'ADA', professor: 'Roberto', hores: 120, style: { backgroundColor: "#F8BBD0" } },
+  { nom: 'PMDM', professor: 'Paco', hores: 100, style: { backgroundColor: "#F48FB1" } },
+  { nom: 'PSP', professor: 'Roberto', hores: 60, style: { backgroundColor: "#F8BBD0" } },
+  { nom: 'SGE', professor: 'Belén', hores: 100, style: { backgroundColor: "#F48FB1" } },
+  { nom: 'Anglés', professor: 'Caterina', hores: 40, style: { backgroundColor: "#F8BBD0" } },
+  { nom: 'EIE', professor: 'Ana', hores: 60, style: { backgroundColor: "#F48FB1" } },
+];
+
 
 const Nom = (textAmostrar, estil) => {
   return (
@@ -26,31 +36,50 @@ const Dades = (arr) => {
     arr.map((elem, index) => {
       return (
         <View key={index}>
-        <TextInput
-          placeholderTextColor={arreu}
-          style={styles[estil]}
-          key={index}
-          placeholder={elem}
-        />
+          <TextInput
+            placeholderTextColor={arreu()}
+            style={styles[estil]}
+            key={index}
+            placeholder={elem}
+          />
         </View>
       )
     })
   )
 
 }
+const camps = () => {
 
+  return (
+    moduls2Dam.map((elem, index) => {
+      return (
+        <View style={elem.style} key={index}>
+          <Text>{index}</Text>
+          <Text>{elem.nom}</Text>
+          <Text>{elem.professor}</Text>
+          <Text>{elem.hores}</Text>
+        </View>
+      )
+    })
+  )
+}
 const Inici = () => {
+  const [mostrar, setMostrar] = useState(false);
+
   return (
     <PaperProvider>
       <View>
-        {Nom("Jorge Sevilla Genoves", { fontSize: 18, fontWidth: "bold", margin: 20 })}
+        {Nom("Jorge Sevilla Genoves", { fontSize: 18, fontWeight: "bold", margin: 20 })}
         {Dades(["Correu", "Nom"])}
-        {mostrarIcona()}
+
+        <Pressable onPress={() => setMostrar(!mostrar)}>
+          {mostrarIcona()}
+        </Pressable>
+        {mostrar && camps()}
       </View>
     </PaperProvider>
   );
-}
-
+};
 
 const styles = StyleSheet.create(
   {
